a30edd65dbe94dbb96818640b3693d21899c424c
ryan-hall <ryan.hall@socrata.com>
2020-02-20 13:36:35 -0800

    init issue145 branch and metadata api file

---
+++ R/metadata_api.R
@@ -0,0 +1,9 @@
+# An interface for reading and writing metadata of Socrata assets
+# 
+# Author: Ryan Hall 2020-02-20
+###############################################################################
+
+# library('httr')       # for access to the HTTP header
+# library('jsonlite')   # for parsing data types from Socrata
+# library('mime')       # for guessing mime type
+# library('plyr')       # for parsing JSON files
